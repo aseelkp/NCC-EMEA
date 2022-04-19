5cba484263a76b8fc8cbc87e2b0f5771c66b8872
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-04-19 15:55:20 +0530

    feat: add achivement image gallery

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,14 +1,14 @@
-import { Carousel } from 'react-carousel-minimal';
-import AchievementImgs from '../../assets/achievements/index'
+import { Carousel } from "react-carousel-minimal";
+import AchievementImgs from "../../assets/achievements/index";
+import styles from "./styles.module.css";
 
 function CarouselApp() {
- const data = [
+  const data = [
     {
       image: AchievementImgs[0],
     },
     {
       image: AchievementImgs[1],
-      
     },
     {
       image: AchievementImgs[2],
@@ -18,50 +18,19 @@ function CarouselApp() {
     },
     {
       image: AchievementImgs[4],
-    }
+    },
   ];
 
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  }
-  const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
   return (
-    <div className="App" >
-      <div style={{ textAlign: "center" }}>
-        <div style={{
-          padding: "0 20px",
-          width: "auto",
-          height: "auto",
-        }}>
-          <Carousel          
-            data={data}
-            time={2000}
-            width="1000px"
-            height="500px"
-            captionStyle={captionStyle}
-            radius="10px"
-            slideNumber={true}
-            slideNumberStyle={slideNumberStyle}
-            captionPosition="bottom"
-            automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="darkgrey"
-            slideImageFit="cover"
-            style={{
-              textAlign: "center",
-              maxWidth: "100%",
-              maxHeight: "100%",
-              margin: "40px auto",
-            }}
-          />
-        </div>
-      </div>
+    <div className={styles.achivementWrapper}>
+      {data.map((img, index) => (
+        <img
+          key={index}
+          src={img.image}
+          alt="Achievement"
+          className={styles.achivementImg}
+        />
+      ))}
     </div>
   );
 }
--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -11,33 +11,9 @@ function AboutNCC() {
     <div className="about section-margin" id="about">
       <h1 className="gradient-text">About</h1>
       <div className="about-container">
-        {aboutData.map((data) => (
-          <About data={data} showMoreHandler={handleShowMore} />
+        {aboutData.map((data, index) => (
+          <About data={data} showMoreHandler={handleShowMore} key={index} />
         ))}
-        {/* <About
-          title="MOTTO OF NCC"
-          text="The need for having motto for the Corps was discussed in the 11th Central Advisory Committee (CAC) meeting held on 11 Aug 1978."
-        />
-        <About
-          title="CORE VALUES"
-          text="The NCC is a responsive, learning and continuously evolving organization. Its activity is guided by certain Core Values that we endeavour to instill among all ranks of the NCC."
-        />
-        <About
-          title="PLEDGE"
-          text="WE THE CADETS OF THE NATIONAL CADET CORPS,DO SOLOEMNLY PLEDGE THAT WE SHALL ALWAYS UPHOLD THE UNITY OF INDIA."
-        />
-        <About
-          title="FLAG"
-          text="The NCC flag for various units of the NCC was first introduced in 1951. The flag was of same pattern, colour and size as was used by various regiments of the Army. "
-        />
-        <About
-          title="NCC SONG"
-          text="Hum Sab Bharatiya Hain, Hum Sab Bharatiya Hain
-                Apni Manzil Ek Hai,
-                Ha, Ha, Ha, Ek Hai,
-                Ho, Ho, Ho, Ek Hai.
-                Hum Sab Bharatiya Hain."
-        /> */}
         {selectedCard && (
           <HoverCard data={selectedCard} closeHandler={handleCloseShowMore} />
         )}
