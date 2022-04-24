a8063faa6ac44001247bdb8e40b2a3ee762019c1
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-04-24 16:35:54 +0530

    feat: add contact details in footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,38 +1,40 @@
-import React from 'react'
-import { SocialMedia } from '../../components'
-import './footer.css'
+import React from "react";
+import { SocialMedia } from "../../components";
+import "./footer.css";
 
 //Material Icons
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import EmailIcon from '@mui/icons-material/Email';
-import CallIcon from '@mui/icons-material/Call';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmailIcon from "@mui/icons-material/Email";
+import CallIcon from "@mui/icons-material/Call";
 
 function Footer() {
-    return (
-        <div className='footer section-padding'>
-            <div className='contact-details'>
-                <div className='contact-details-wrapper'>
-                    <div className='address contact-details-child'>
-                        <LocationOnIcon fontSize='large' sx={{color: '#fff'}} />
-                        <p>EMEA COLLEGE OF ARTS AND SCIENCE ,KONDOTTY <br /> MALAPPURAM, Kerala, 673637</p>
-                    </div>
-                    <div className='email contact-details-child'>
-                        <EmailIcon fontSize='large' sx={{color: '#fff'}} />
-                        <p> <br /></p>
-                    </div>
-                    <div className='phone contact-details-child'>
-                        <CallIcon fontSize='large' sx={{color: '#fff'}} />
-                        <p></p>
-                    </div>
-                </div>
-
-            </div>
-            <SocialMedia />
-            <div className='footer-copyright'>
-                <p>© 2022 NCC EMEA. All rights reserved.</p>
-            </div>
+  return (
+    <div className="footer section-padding">
+      <div className="contact-details">
+        <div className="contact-details-wrapper">
+          <div className="address contact-details-child">
+            <LocationOnIcon fontSize="large" sx={{ color: "#fff" }} />
+            <p>
+              EMEA COLLEGE OF ARTS AND SCIENCE ,KONDOTTY <br /> MALAPPURAM,
+              Kerala, 673637
+            </p>
+          </div>
+          <div className="email contact-details-child">
+            <EmailIcon fontSize="large" sx={{ color: "#fff" }} />
+            <p> nccemeacollege01@gmail.com </p>
+          </div>
+          <div className="phone contact-details-child">
+            <CallIcon fontSize="large" sx={{ color: "#fff" }} />
+            <p>+91 9400600786</p>
+          </div>
         </div>
-    )
+      </div>
+      <SocialMedia />
+      <div className="footer-copyright">
+        <p>© 2022 NCC EMEA. All rights reserved.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
